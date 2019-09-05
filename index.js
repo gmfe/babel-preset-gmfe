93007096c89b6abd5ef90678b82ddad705ff5498
linxueyu <lin2006yuo@gmail.com>
2019-09-05 12:05:44 +0800

    根据环境引入dll

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 const { declare } = require('@babel/helper-plugin-utils')
 
+const DLL_ENV = process.env.DLL_ENV === 'true'
+const plugins = [
+  [
+    require('@babel/plugin-proposal-decorators'),
+    {
+      legacy: true
+    }
+  ],
+  require('@babel/plugin-syntax-dynamic-import'),
+  [
+    require('@babel/plugin-proposal-class-properties'),
+    { loose: true }
+  ],
+  require('@babel/plugin-proposal-function-bind'),
+  require('@babel/plugin-transform-runtime')
+]
+// 非dll的时候引入react-hot-loader/babel
+if(!DLL_ENV) plugins.push('react-hot-loader/babel')
+
 module.exports = declare((api, options) => {
   // 暂时没有提供 options 配置
   // see docs about api at https://babeljs.io/docs/en/config-files#apicache
@@ -16,21 +35,6 @@ module.exports = declare((api, options) => {
         }
       ]
     ],
-    plugins: [
-      [
-        require('@babel/plugin-proposal-decorators'),
-        {
-          legacy: true
-        }
-      ],
-      'react-hot-loader/babel',
-      require('@babel/plugin-syntax-dynamic-import'),
-      [
-        require('@babel/plugin-proposal-class-properties'),
-        { loose: true }
-      ],
-      require('@babel/plugin-proposal-function-bind'),
-      require('@babel/plugin-transform-runtime')
-    ]
+    plugins
   }
 })
