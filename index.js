0f454a34b79dc77001d8356c2eb6942302902178
LaamGinghong <351390560@qq.com>
2020-02-04 12:13:31 +0800

    ts-babel

--- index.js
+++ index.js
@@ -1,38 +1,45 @@
-const { declare } = require('@babel/helper-plugin-utils')
+const {declare} = require('@babel/helper-plugin-utils')
 
 module.exports = declare((api, options) => {
-  // 暂时没有提供 options 配置
-  // see docs about api at https://babeljs.io/docs/en/config-files#apicache
-  api.assertVersion('^7.0.0')
+    // 暂时没有提供 options 配置
+    // see docs about api at https://babeljs.io/docs/en/config-files#apicache
+    api.assertVersion('^7.0.0')
 
-  return {
-    sourceType: 'unambiguous', // 自动推断编译的模块类型(cjs,es6)
-    presets: [
-      require('@babel/preset-react'),
-      [
-        require('@babel/preset-env'),
-        {
-          modules: false
-        }
-      ]
-    ],
-    plugins: [
-      [
-        require('@babel/plugin-proposal-decorators'),
-        {
-          legacy: true
-        }
-      ],
-      'react-hot-loader/babel',
-      require('@babel/plugin-syntax-dynamic-import'),
-      [
-        require('@babel/plugin-proposal-class-properties'),
-        { loose: true }
-      ],
-      require('@babel/plugin-proposal-function-bind'),
-      require('@babel/plugin-transform-runtime'),
-      'styled-jsx/babel',
-      require('@babel/plugin-proposal-optional-chaining')
-    ]
-  }
+    return {
+        sourceType: 'unambiguous', // 自动推断编译的模块类型(cjs,es6)
+        presets: [
+            require('@babel/preset-react'),
+            [
+                require('@babel/preset-env'),
+                {
+                    modules: false
+                }
+            ],
+            [require('@babel/preset-typescript'), {
+                isTSX: true,
+                allExtensions: true,
+
+            }]
+        ],
+        plugins: [
+            [
+                require('@babel/plugin-proposal-decorators'),
+                {
+                    legacy: true
+                }
+            ],
+            'react-hot-loader/babel',
+            require('@babel/plugin-syntax-dynamic-import'),
+            [
+                require('@babel/plugin-proposal-class-properties'),
+                {loose: true}
+            ],
+            require('@babel/plugin-proposal-object-rest-spread'),
+            require('@babel/plugin-proposal-function-bind'),
+            require('@babel/plugin-transform-runtime'),
+            'styled-jsx/babel',
+            require('@babel/plugin-proposal-optional-chaining'),
+            require('@babel/')
+        ]
+    }
 })
